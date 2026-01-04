7614e019fe44b9b44b72117e4d271a83ef42392d
BunnyBharath91 <bunnybharath917@gmail.com>
2026-01-05 05:23:22 +0530

    Create basic routes

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,35 +1,26 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import React from "react";
+import { Routes, Route } from "react-router-dom";
+import Home from "./pages/Home";
+import MyProjects from "./pages/MyProjects";
+import Preview from "./pages/Preview";
+import Pricing from "./pages/Pricing";
+import Projects from "./pages/Projects";
+import View from "./pages/View";
+import Community from "./pages/Community";
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    <Routes>
+      <Route path="/" element={<Home />} />
+      <Route path="/pricing" element={<Pricing />} />
+      <Route path="/projects/:projectId" element={<Projects />} />
+      <Route path="/projects" element={<MyProjects />} />
+      <Route path="/preview/:projectId" element={<Preview />} />
+      <Route path="/preview/:projectId/:versionId" element={<Preview />} />
+      <Route path="/community" element={<Community />} />
+      <Route path="/view/:projectId" element={<View />} />
+    </Routes>
+  );
 }
 
-export default App
+export default App;
